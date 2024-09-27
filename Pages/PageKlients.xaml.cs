5bff19031d96b07dc173dfe2c5d8b1010207f167
elizaveta05 <elozovetalyapynova05@gmail.com>
2024-09-27 08:58:05 +0700

    Разработка вывода данных клиента для редактирования

--- Pages/PageKlients.xaml.cs
+++ Pages/PageKlients.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Authoservice.Model;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,18 +23,41 @@ namespace Authoservice.Pages
     public partial class PageKlients : Page
     {
         public Client client;
+        private readonly string imagesBasePath = @"C:\Users\elozo\OneDrive\Рабочий стол\4 курс\МДК.02.02 Инструментальные средства разработки программного обеспечения\Автосервис - клиенты\Authoservice\Images";
+
         public PageKlients(Client client)
         {
             InitializeComponent();
             this.client = client;
-            LoadData(client);
+            if(client != null)
+            {
+                LoadData(client);
+            }
         }
 
         private void LoadData(Client client)
         {
             try
             {
-                tbFirstName.Text = client.FirstName;
+                if (client.PhotoPath != null)
+                {
+                    string dbPath = client.PhotoPath.Replace("/", "\\").Trim();
+                    string fullPath = System.IO.Path.Combine(imagesBasePath, dbPath.TrimStart('\\'));
+
+                    if (File.Exists(fullPath))
+                    {
+                        BitmapImage bitmap = new BitmapImage(new Uri(fullPath, UriKind.Absolute));
+                        imageClient.Source = bitmap;
+                    }
+                    else
+                    {
+                        string defaultImagePath = System.IO.Path.Combine(imagesBasePath, "service_logo.png");
+                        BitmapImage bitmap = new BitmapImage(new Uri(defaultImagePath, UriKind.Absolute));
+                        imageClient.Source = bitmap;
+                    }
+                }
+
+                    tbFirstName.Text = client.FirstName;
                 tbLastName.Text = client.LastName;
                 tbPatronymic.Text = client.Patronymic;
                 tbEmail.Text = client.Email;
@@ -48,11 +72,9 @@ namespace Authoservice.Pages
                 {
                     rbFemale.IsChecked = true;
                 }
-                var tags = Number2Entities.GetContext().Client
-                                .Include("Tag")
-                                .FirstOrDefault(c => c.ID == client.ID)?.Tag.ToList();
 
-                gridTag.ItemsSource = tags; 
+                gridTag.ItemsSource = Number2Entities.GetContext().Client
+                    .Include("Tag").Where(c=>c.ID == client.ID).ToList();
             }
             catch (Exception ex)
             {
